8a2dc7d7b593b588af93e91faa8f012811d18b95
SzymonT2006 <156914033+SzymonT2006@users.noreply.github.com>
2024-11-06 13:04:20 +0100

    Add files via upload

--- baza.php
+++ baza.php
@@ -8,7 +8,30 @@
 <body>
 
 <?php
-$connect = mysqli_connect() 
+$connect = mysqli_connect( "localhost","root","","KINO4tig1");
+if(mysqli_connect_errno()){
+echo"<p>MYSQL CONNECTION FAILED</p>";
+}
+else{
+    echo"<p>CONNCETION SUCCESS";
+}
+
+$query = "SELECT * FROM Filmy;";
+if($result = mysqli_query($connect,$query)){
+echo"<table border = 1 >";
+echo"<tr><th>ID</th><th>Tytuł</th><th>Rezyser</th><th>czas trwania</th></tr>";
+
+foreach(mysqli_fetch_all(result: $result)as $row){
+echo"<tr><td>".$row[0]."</td><td>".$row[1]."</td><td>".$row[2]."</td><td>".$row[3]."</td>";
+
+
+}
+echo "</table>";
+
+
+}
+mysqli_close($connect);
+
 ?>
 
 </body>
